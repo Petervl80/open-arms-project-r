657db53023ce1249751a88b4a5818f4f8d9e681c
Petervl80 <pedro.lapa1911@gmail.com>
2026-04-15 17:29:41 -0300

    feat: add Child Record Base Controller

---
+++ app/controllers/api/v1/child_record_base_controller.rb
@@ -0,0 +1,24 @@
+module Api
+  module V1
+    class ChildRecordBaseController < CrudBaseController
+      
+      #before_action :require_technical_team!
+
+      def index
+        records = model_class.all
+
+        if params[:child_id].present?
+          records = records.where(child_id: params[:child_id])
+        end
+
+        records = records.order(date: :desc)
+
+        if respond_to?(:includes_for_index, true)
+          records = records.includes(includes_for_index)
+        end
+
+        render json: records, include: respond_to?(:includes_for_index, true) ? includes_for_index : [], status: :ok
+      end
+    end
+  end
+end
