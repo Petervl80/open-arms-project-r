ce9db7f858036b77d34734f4b122a792c41bdefd
Petervl80 <pedro.lapa1911@gmail.com>
2026-04-17 14:44:10 -0300

    feat: Add a default JSON format

--- app/controllers/api/v1/children_controller.rb
+++ app/controllers/api/v1/children_controller.rb
@@ -4,58 +4,11 @@ module Api
       
       #before_action :require_technical_team!, only: [:create, :update, :destroy]
 
-      def index
-        records = model_class.respond_to?(:kept) ? model_class.kept : model_class.all
-        records = records.order(:full_name).includes(:sex_type, :race_type, :process_type)
-
-        requested_limit = params.fetch(:limit, 20).to_i
-        safe_limit = [requested_limit, 100].min
-        @pagy, paginated_records = pagy(records, limit: safe_limit)
-
-        render json: {
-          data: ChildBlueprint.render_as_hash(paginated_records, view: :extended),
-          meta: pagy_metadata(@pagy)
-        }, status: :ok
-      end
-
-      def show
-        render json: {
-          data: ChildBlueprint.render_as_hash(@record, view: :extended)
-        }, status: :ok
-      end
-
-      def create
-        @record = Child.new(record_params)
-        @record.updated_by_id = @current_user.id if @record.respond_to?(:updated_by_id=)
-
-        # Injeta a auditoria em cada contato que está a ser criado junto com a criança
-        @record.child_contacts.each do |contact|
-          contact.updated_by_id = @current_user.id if contact.respond_to?(:updated_by_id=)
-        end
-
-        if @record.save
-          render json: @record, status: :created
-        else
-          render json: { errors: @record.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
-
-      def update
-        @record.updated_by_id = @current_user.id if @record.respond_to?(:updated_by_id=)
-        @record.assign_attributes(record_params)
-
-        @record.child_contacts.each do |contact|
-          contact.updated_by_id = @current_user.id if contact.respond_to?(:updated_by_id=)
-        end
-
-        if @record.save
-          render json: @record, status: :ok
-        else
-          render json: { errors: @record.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
-
       private
+
+      def includes_for_index
+        [:sex_type, :race_type, :process_type]
+      end
 
       def record_params
         params.require(:child).permit(
--- app/controllers/api/v1/crud_base_controller.rb
+++ app/controllers/api/v1/crud_base_controller.rb
@@ -6,56 +6,54 @@ module Api
       def index
         records = model_class.respond_to?(:kept) ? model_class.kept : model_class.all
 
-        @pagy, paginated_records = pagy(records)
+        records = records.includes(includes_for_index) if respond_to?(:includes_for_index, true)
+
+        requested_limit = params.fetch(:limit, 20).to_i
+        safe_limit = [requested_limit, 100].min
+
+        @pagy, paginated_records = pagy(records, limit: safe_limit)
 
         render json: {
-          data: paginated_records,
+          data: render_with_blueprint(paginated_records),
           meta: pagy_metadata(@pagy)
         }, status: :ok
       end
 
       def show
-        render json: @record, status: :ok
+        render json: {
+          data: render_with_blueprint(@record, view: blueprint_show_view)
+        }, status: :ok
       end
 
       def create
         @record = model_class.new(record_params)
-        
-        if @record.respond_to?(:updated_by_id=)
-          @record.updated_by_id = @current_user.id
-        end
+        @record.updated_by_id = @current_user.id if @record.respond_to?(:updated_by_id=)
 
         if @record.save
-          render json: @record, status: :created
+          render json: { data: render_with_blueprint(@record, view: blueprint_show_view) }, status: :created
         else
           render json: { errors: @record.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def update
-        if @record.respond_to?(:updated_by_id=)
-          @record.updated_by_id = @current_user.id
-        end
+        @record.updated_by_id = @current_user.id if @record.respond_to?(:updated_by_id=)
+        @record.assign_attributes(record_params)
 
-        if @record.update(record_params)
-          render json: @record, status: :ok
+        if @record.save
+          render json: { data: render_with_blueprint(@record, view: blueprint_show_view) }, status: :ok
         else
           render json: { errors: @record.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def destroy
-        
         if @record.respond_to?(:discard)
           @record.discard
-          
-          if @record.respond_to?(:updated_by_id=)
-            @record.update_column(:updated_by_id, @current_user.id)
-          end
+          @record.update_column(:updated_by_id, @current_user.id) if @record.respond_to?(:updated_by_id=)
         else
           @record.destroy
         end
-        
         head :no_content
       end
 
@@ -72,6 +70,24 @@ module Api
       # Obriga o controller filho a definir quais parâmetros são permitidos
       def record_params
         raise NotImplementedError, "Você precisa definir o método 'record_params' no controller filho."
+      end
+
+      def blueprint_class
+        "#{model_class.name}Blueprint".constantize
+      rescue NameError
+        nil
+      end
+
+      def blueprint_show_view
+        :extended 
+      end
+
+      def render_with_blueprint(data, view: :default)
+        if blueprint_class
+          blueprint_class.render_as_hash(data, view: view)
+        else
+          data.as_json
+        end
       end
     end
   end
